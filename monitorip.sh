71be3ae83f9a8c73e2b6f7e8f5892a7413b4e7d3
trohit <rohit123@gmail.com>
2016-07-21 22:41:13 +0530

    corrected for rasp pi devices

--- monitorip.sh
+++ monitorip.sh
@@ -1,60 +1,79 @@
 #!/bin/bash
+#set -x
 # Sample Usage
 # monitorip.sh 192.168.1.5
 #
-
 ##############################################################################
 #  *****     Configuration parameters that need to be modified     *****
 ##############################################################################
 AFTER_UP_DELAY=10
 AFTER_DOWN_DELAY=3
-INTERFACE=wlp3s0
+INTERFACE=`/sbin/route -n | grep "^0.0.0.0" | awk {'print $8'}` # ex. wlan0
+
 ##############################################################################
 # how many secs to wait before declaring device as down
 ##############################################################################
-HYSTERISIS=10
-
+HYSTERISIS=5
 # exit on Ctrl-C
 trap "exit" INT
-
 # Needs an IP to monitor on the network
 if [ $# -ne 1 ]
 then
 	echo "Usage: $(basename $0) w.x.y.z"
 	exit 1
 fi
-
 DEVICE=$1 
-CMD="arping $DEVICE -I $INTERFACE -c $HYSTERISIS -f -w $HYSTERISIS"
-
+# to bypass using sudo on rasp pi like devices do:
+# sudo chmod u+s /usr/bin/arping
+L2_CMD="sudo arping $DEVICE -I $INTERFACE -c $HYSTERISIS -f -w $HYSTERISIS"
+L3_CMD="ping $DEVICE -c 1 -w $HYSTERISIS"
 # housekeeping - used internally
-PINGRES=0
+PINGRES=-1
 PREVSTATE=0
-
-
-function ping_dev
+#DEBUGLOG=/tmp/out
+DEBUGLOG=/dev/null
+function l2ping
 {
-		$CMD > /dev/null 2>&1
-		#$CMD >> /tmp/log 2>&1
+		$L2_CMD >> $DEBUGLOG 2>&1
+		#$L2_CMD >> /tmp/log 2>&1
 		res=$?
 		if [ $res -eq 0 ]; then
 			#echo Success
-		    PINGRES=0
+		    	PINGRES=0
 		else 
+			# arping sometimes behaves strangely
+			# be doubly sure its down
+			# try a layer 3 ping
+			l3ping
+			if [ $res -eq 0 ]; then
+				#echo Success
+				PINGRES=0
+			else	
+				#echo Failure
+				PINGRES=1
+			fi	
+		fi
+}
+function l3ping
+{
+		$L3_CMD >> $DEBUGLOG 2>&1
+		res=$?
+		if [ $res -eq 0 ]; then
+			#echo Success
+			PINGRES=0
+		else
 			#echo Failure
 			PINGRES=1
 		fi
 }
-
 # main
 while true
 do
-	#CMD="./checkfordev.sh $DEVICE"
-	#$CMD
-	ping_dev
+	l2ping
 	#PINGRES=$?
 	if [ $PINGRES -eq 0 ]; then
 		#echo "$(date) : $DEVICE is on WLAN"
+		/usr/bin/mosquitto_pub -h localhost -t "rohithall/power" -m "on"
 		if [ $PINGRES -ne $PREVSTATE ]; then
 			echo -ne "\n$(date) : $DEVICE came online"
 		else
@@ -64,6 +83,7 @@ do
 		#exit 0
 	else
 		#echo "$(date) : $DEVICE is off the WLAN"
+		/usr/bin/mosquitto_pub -h localhost -t "rohithall/power" -m "off"
 		if [ $PINGRES -ne $PREVSTATE ]; then
 			echo -ne "\n$(date) : $DEVICE went offline"
 		else
